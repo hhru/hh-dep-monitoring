af5d6f1e5bea3e612919a9c670c6d4e180fb73a9
Afftobus <filippoff@mail.ru>
2019-06-03 23:29:29 +0300

    back-48 add artefact info to event DTO

--- hh-dep-monitoring-service/src/main/java/ru/hh/school/depmonitoring/dao/EventDao.java
+++ hh-dep-monitoring-service/src/main/java/ru/hh/school/depmonitoring/dao/EventDao.java
@@ -3,8 +3,13 @@ package ru.hh.school.depmonitoring.dao;
 import ru.hh.school.depmonitoring.entities.Event;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface EventDao extends Dao<Event, Integer> {
     Optional<LocalDateTime> getLastEventForRepository(Long repositoryId);
+
+    List<Event> getAllEventsForRepository(long repositoryId);
+
+    List<Event> getLastEventsForRepository(long repositoryId, int eventsCount);
 }
