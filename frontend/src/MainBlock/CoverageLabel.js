8f9e57d22e7694e27d14d633ede2c3bfe65a7127
Алексей Сулим <Suhoy_Vasya@mail.ru>
2019-06-04 11:21:48 +0300

    front-26 add coverage to repositories

---
+++ frontend/src/MainBlock/CoverageLabel.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { withStyles } from '@material-ui/core/styles';
+import Chip from '@material-ui/core/Chip';
+
+import { chip } from 'Utils/commonStyles';
+
+const styles = () => ({
+    coverageMark: {
+        ...chip,
+        marginLeft: '-2px',
+    },
+    coverageBigMark: {
+        marginLeft: '12px',
+    },
+    chipIcon: {
+        fontSize: 20,
+    },
+});
+
+
+
+function CoverageLabel({ classes, value, big }) {
+    return (
+        <Chip
+            label={value+'%'}
+            className={big ? classes.coverageMark : classes.coverageBigMark}
+        />
+    );
+}
+
+CoverageLabel.propTypes = {
+    classes: PropTypes.object.isRequired,
+    value: PropTypes.string.isRequired,
+    big: PropTypes.bool,
+};
+
+export default withStyles(styles)(CoverageLabel);
